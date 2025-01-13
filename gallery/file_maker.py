8004e54e65053227adbaed3448eee8f80f2e904d
Crafty-Star <anishsarkar@proofschool.org>
2025-01-12 16:56:04 -0800

    delete 366 files and update 2 files

--- gallery/file_maker.py
+++ gallery/file_maker.py
@@ -1,201 +1,41 @@
 import os
+from datetime import datetime
+import time
 
-def list_images_in_folders():
-    cwd = os.getcwd()
-    folder_image_dict = {}
-    for item in os.listdir(cwd):
-        if os.path.isdir(os.path.join(cwd, item)):
-            folder_path = os.path.join(cwd, item)
-            image_list = []
-            for file in os.listdir(folder_path):
-                if file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
-                    image_list.append(file)
-            folder_image_dict[item] = image_list
-
-    return folder_image_dict
+def rename_photos(folder_path):
+    # Get all files in the directory
+    files = []
+    for filename in os.listdir(folder_path):
+        # Check if the file is an image (you can add more extensions if needed)
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif', '.bmp')):
+            filepath = os.path.join(folder_path, filename)
+            # Get creation time and add to list with filepath
+            creation_time = os.path.getctime(filepath)
+            files.append((creation_time, filepath))
+    
+    # Sort files by creation time
+    files.sort(key=lambda x: x[0])
+    
+    # Rename files
+    for index, (creation_time, filepath) in enumerate(files, 1):
+        # Get the file extension
+        _, ext = os.path.splitext(filepath)
+        # Create new filename
+        new_name = f"photo_{index}{ext}"
+        new_filepath = os.path.join(folder_path, new_name)
+        
+        try:
+            os.rename(filepath, new_filepath)
+            # Get the creation time as a readable string
+            time_str = datetime.fromtimestamp(creation_time).strftime('%Y-%m-%d %H:%M:%S')
+            print(f"Renamed: {os.path.basename(filepath)} → {new_name} (Created: {time_str})")
+        except Exception as e:
+            print(f"Error renaming {filepath}: {e}")
 
 # Example usage
 if __name__ == "__main__":
-    images_in_folders = list_images_in_folders()
-    big = open('./index.html', 'w')
-    content = """---
-layout: page
-title: "ALBUMS"
-description: "archive"
-active: gallery
-header-img: "img/gallery.jpg"
-images:"""
-    for folder, images in images_in_folders.items():
-        try:
-            img = images[0]
-        except:
-            continue
-
-        content += f"""\n- image_path: /gallery/{folder}/{img}
-  gallery-folder: /gallery/{folder}/
-  gallery-name: {folder}"""
-
-        f = open(f"{folder}/index.html", "w")
-        header = f"""---
-layout: page
-title: "{folder}"
-active: gallery
-header-img: "gallery/{folder}/{img}"
-album-title: "{folder}"
-images:"""
-        for image in images:
-            header += f"\n- image_path: /gallery/{folder}/{image}"
-        header += '\n---'
-        header += """
-<html class="no-js" lang="en">
-<head>
-	<meta content="charset=utf-8">
-</head>
-
-    <body>
- 
-	<section id="content" role="main">
-		<div class="wrapper">
-	<br><br>
-			<h2>{{page.album-title}}</h2>
-
-
-			<!-- Gallery __-->
-			<div class="gallery masonry-gallery">
-		
-{% for image in page.images %}  		
-
-				<figure class="gallery-item">
-					<header class='gallery-icon'>
-
-<a href="{{ site.url }}{{ site.baseurl }}{{ image.image_path }}" class="popup"  title="{{ image.caption }}" data-caption="{{ image.copyright }}">
-<img src="{{ site.url }}{{ site.baseurl }}{{ image.image_path }}"></a>
-						
-					</header>	
-					<figcaption class='gallery-caption'>
-						<div class="entry-summary" id="{{ image.caption }}">
-							<h3>{{image.caption}}</h3>
-							<p>{{image.copyright}}</p>
-						</div>
-					</figcaption>
-				</figure>
-				
-{% endfor %}
-
-			</div>
-
-		</div><!-- END .wrapper -->
-	</section>
-
-<!-- jQuery -->    
-
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<!-- include image popups -->
-<script src="{{ site.baseurl }}/js/jquery.magnific-popup.js"></script>
-
-<script type="text/javascript">
-      $(document).ready(function($) {
-        $('a.popup').magnificPopup({
-         type: 'image',
-	  gallery:{
-         enabled:true,
-         navigateByImgClick: true,
-         preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-       },
-image: {
-      titleSrc: function(item) {
-              return item.el.attr('title') + '&nbsp;' + item.el.attr('data-caption');
-            }
-        }
-          // other options
-      });
-});
-    </script>
-
-<script src="{{ site.baseurl }}/js/retina.min.js"></script>
-<!-- include Masonry -->
-<script src="{{ site.baseurl }}/js/isotope.pkgd.min.js"></script> 
-<!-- include mousewheel plugins -->
-<script src="{{ site.baseurl }}/js/jquery.mousewheel.min.js"></script>
-<!-- include carousel plugins -->
-<script src="{{ site.baseurl}}/js/jquery.tinycarousel.min.js"></script>
-<!-- include svg line drawing plugin -->
-<script src="{{ site.baseurl }}/js/jquery.lazylinepainter.min.js"></script>
-<!-- include custom script -->
-<script src="{{ site.baseurl }}/js/scripts.js"></script>
-<!-- Modernizr -->
- <script src="{{ site.baseurl }}/js/modernizr.js"></script>
-
-    
-</body></html>"""
-        # here
-        f.write(header)
-        f.close()
-    content += """\n---
-
-<html class="no-js" lang="en">
-<head>
-	<meta content="charset=utf-8">
-    <link rel="stylesheet" href="{{ "/css/selena.css" | prepend: site.baseurl }}">
-</head>
-
-
-    <body class="gallery">
- 
-	<section id="content" role="main">
-		<div class="wrapper">
-	
-			<h2>{{page.title}}</h2>
-			
-			<!-- Gallery __-->
-			<div class="gallery masonry-gallery">
-				
-{% for image in page.images %}  	
-
-	               <figure class="gallery-item">
-                         <figure class="effect-selena">
-					<header class='gallery-icon'>
-		       
-<a href="{{ site.url }}{{ site.baseurl }}{{ image.gallery-folder }}">
-<img src="{{ site.url }}{{ site.baseurl }}{{ image.image_path }}"></a>
-
-					</header>	
-					<figcaption class='gallery-caption'>
-						<div class="entry-summary">
-							<h3>{{image.gallery-name}}</h3>
-							<p>{{image.gallery-date}}</p>
-						</div>
-					</figcaption>
-                       </figure>
-				</figure>
-							
-{% endfor %}		
-				
-			</div>
-			
-		</div><!-- END .wrapper -->
-	</section>
-
-
-<br>
-<!-- jQuery -->    
-
-<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-<script src="{{ site.baseurl }}/js/retina.min.js"></script>
-<!-- include Masonry -->
-<script src="{{ site.baseurl }}/js/isotope.pkgd.min.js"></script> 
-<!-- include mousewheel plugins -->
-<script src="{{ site.baseurl }}/js/jquery.mousewheel.min.js"></script>
-<!-- include carousel plugins -->
-<script src="{{ site.baseurl}}/js/jquery.tinycarousel.min.js"></script>
-<!-- include svg line drawing plugin -->
-<script src="{{ site.baseurl }}/js/jquery.lazylinepainter.min.js"></script>
-<!-- include custom script -->
-<script src="{{ site.baseurl }}/js/scripts.js"></script>
-<!-- Modernizr -->
- <script src="{{ site.baseurl }}/js/modernizr.js"></script>
-
-</body></html>"""
-    big.write(content)
-    big.close()
-
+    folder_path = input("Enter the folder path containing the photos: ")
+    if os.path.exists(folder_path):
+        rename_photos(folder_path)
+    else:
+        print("Invalid folder path!")
